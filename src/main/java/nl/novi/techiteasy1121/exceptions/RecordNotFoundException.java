0f8d07813ab2b66b0b0b11a5500825a2d168d180
MRensen <markrensen@hotmail.com>
2024-02-05 16:39:02 +0100

    SerialVersionUUID verwijderd

--- src/main/java/nl/novi/techiteasy1121/exceptions/RecordNotFoundException.java
+++ src/main/java/nl/novi/techiteasy1121/exceptions/RecordNotFoundException.java
@@ -2,8 +2,6 @@ package nl.novi.techiteasy1121.exceptions;
 
 // Deze klasse vormt onze custom exception
 public class RecordNotFoundException extends RuntimeException {
-    // Deze waarde is enkel nuttig voor serialization/deserialization. Dit hoeft niet in je code.
-    private static final long serialVersionUID = 1L;
 
     // De exception zonder message
     public RecordNotFoundException() {
